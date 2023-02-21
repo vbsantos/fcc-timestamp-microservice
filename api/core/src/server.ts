392ebc65371785953df4c9087ce3f3563394a343
Vinícius Bohrer dos Santos <vinicius.bohrer@voalle.com.br>
2023-02-21 14:33:23 -0300

    First Commit

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -1,40 +1,38 @@
-
-import express from 'express';
+import express from "express";
 
 var app = express();
 
 const users = [
   {
-      id: 1,
-      name: "Lucas"
+    id: 1,
+    name: "Lucas",
   },
   {
-      id: 2,
-      name: "Eric"
+    id: 2,
+    name: "Eric",
   },
   {
-      id: 3,
-      name: "Ana"
+    id: 3,
+    name: "Ana",
   },
 ];
 
-
-app.get('/', function(req:any, res:any){
-  res.send('Hello!');
+app.get("/", (req: any, res: any) => {
+  res.send("Hello!");
 });
 
-app.get('/users', function(req:any, res:any){
+app.get("/users", (req: any, res: any) => {
   res.send(users);
 });
 
-app.get('/users/:userId', function(req:any, res:any){
+app.get("/users/:userId", (req: any, res: any) => {
   const user = users.filter((user) => user.id == req.params.userId);
   res.send(user);
 });
 
 if (!module.parent) {
   app.listen(3000);
-  console.log('Express started on port 3000');
+  console.log("Express started on port 3000");
 }
 
 export default app;
